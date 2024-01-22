5865ede8e2c13131c0584bbbae82920c9a513893
AMKolev22 <amkolev22@codingburgas.bg>
2024-01-22 07:35:50 +0200

    Add dashboard

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'custom': '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)',
+      }
+    },
   },
   plugins: [],
 }
