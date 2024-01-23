faca746a96cfe63547d934747903da81222e0fc2
AMKolev22 <amkolev22@codingburgas.bg>
2024-01-23 07:11:21 +0200

    Add demo form for testing

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -9,8 +10,15 @@ module.exports = {
     extend: {
       boxShadow: {
         'custom': '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)',
+      },
+      colors:{
+        'black-accent': '#0a0a0a',
+        'font-color': '#121212',
+        'dark-secondary': '#888888',
+        'dark-primary': '#F3F8FF',
+        'white-accent': '#efefef',
       }
     },
   },
   plugins: [],
-}
+})
