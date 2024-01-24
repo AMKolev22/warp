4f9b8fd603c996403cd7da98845143f175616f3e
AMKolev22 <amkolev22@codingburgas.bg>
2024-01-24 06:56:10 +0200

    Add signin page and start work on auth

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,5 +1,9 @@
 import "../styles/globals.css";
-
+import {SessionProvider} from "next-auth/react"
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+    )
   }
--- web/pages/_document.js
+++ web/pages/_document.js
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin = "true" />
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet" />
       <body>
         <Main />
         <NextScript />
