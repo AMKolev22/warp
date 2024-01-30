b8f4177348fc0d105864e553b0924d097287f7af
AMKolev22 <amkolev22@codingburgas.bg>
2024-01-30 04:10:32 +0200

    Temp gcp api upload / fs-extra experiment

--- web/lib/gcp-create.ts
+++ web/lib/gcp-create.ts
@@ -1,7 +1,27 @@
 import { Storage } from "@google-cloud/storage";
+import fs from "fs-extra"
 
-const bucketId : string = "warp-storage";
 
-export function createFile (idProject : string) {
-    console.log(`project id ${idProject}`);
+const storage = new Storage();
+
+export async function uploadFile(bucketID : string = "warp-storage", id : string) {
+    let destination : string | null = `../projects/${id}`;
+    await storage.bucket(bucketID).upload(id, {
+        gzip: true,
+        destination: destination,
+    });
+  fs.remove(destination, err => {
+        console.log(`${err}`)
+        return err;
+    })
+  console.log(`Successfully uploaded.`);
+}
+
+export async function createFile(id : string){
+    let destination : string | null = `../projects/${id}/index.html`;
+    destination = `../projects/${id}`;
+    fs.createFile(destination, err => {
+        console.log(`${err}`)
+        return err;
+    })
 }
